5d464ceb070550bfc74b145b00693fd9f546c513
Ryan Varley <ryanjvarley@gmail.com>
2013-05-01 21:17:15 +0100

    works in sidebar

--- section.php
+++ section.php
@@ -5,7 +5,7 @@
     Author URI: http://ryanvarley.co.uk
     Description: Oraganise boxes with a picture with overlaying text
     Class Name: FeatPicBoxes
-    Workswith: templates, main, header, morefoot, footer, sidebar
+    Workswith: templates, main, header, morefoot, footer, sidebar1, sidebar2, sidebar_wrap
     Cloning: true
 */
 
